f2a910a74d903a1ea5d0de9f044da7866c9c125b
Keith <ko.keith@hotmail.com>
2020-09-15 16:51:57 -0400

    add test page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -4,4 +4,6 @@ class WelcomeController < ApplicationController
 
     end
 
+    def test
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root 'welcome#home'
-
   get 'resume', to: "welcome#home"
+  get 'test', to: "welcome#test"
 end
